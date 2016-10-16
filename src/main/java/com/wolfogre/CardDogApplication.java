81efd595338bea03662d2179b3e2e9bb7aaae4df
Jason Song <i@wolfogre.com>
2016-10-16 13:05:44 +0800

    First commit

---
+++ src/main/java/com/wolfogre/CardDogApplication.java
@@ -0,0 +1,12 @@
+package com.wolfogre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardDogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardDogApplication.class, args);
+	}
+}
