84ca50eed12fa77a8623eb07318d027facd4713e
Jason Song <i@wolfogre.com>
2016-11-14 15:27:56 +0800

    I have no idea what to do

--- src/main/java/com/wolfogre/Main.java
+++ src/main/java/com/wolfogre/Main.java
@@ -1,9 +1,5 @@
 package com.wolfogre;
 
-import com.wolfogre.service.AnalysisTimeMoneyInDay;
-import com.wolfogre.service.AnalysisTimeMoneyInDayOfPoor;
-import com.wolfogre.service.ChartOfAnalysisTimeMoneyInDay;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,26 +8,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class Main implements CommandLineRunner {
-
-    private final AnalysisTimeMoneyInDay analysisTimeMoneyInDay;
-    private final ChartOfAnalysisTimeMoneyInDay chartOfAnalysisTimeMoneyInDay;
-    private final AnalysisTimeMoneyInDayOfPoor analysisTimeMoneyInDayOfPoor;
-
-    @Autowired
-    public Main(AnalysisTimeMoneyInDay analysisTimeMoneyInDay,
-                ChartOfAnalysisTimeMoneyInDay chartOfAnalysisTimeMoneyInDay,
-                AnalysisTimeMoneyInDayOfPoor analysisTimeMoneyInDayOfPoor) {
-        this.analysisTimeMoneyInDay = analysisTimeMoneyInDay;
-        this.chartOfAnalysisTimeMoneyInDay = chartOfAnalysisTimeMoneyInDay;
-        this.analysisTimeMoneyInDayOfPoor = analysisTimeMoneyInDayOfPoor;
-    }
-
-
     @Override
     public void run(String... strings) throws Exception {
-        //analysisTimeMoneyInDay.run(strings);
-        chartOfAnalysisTimeMoneyInDay.run(strings);
-        //Thrown when code that is dependent on a keyboard, display, or mouse is called in an environment that does not support a keyboard, display, or mouse.
-        //analysisTimeMoneyInDayOfPoor.run(strings);
+
     }
 }
